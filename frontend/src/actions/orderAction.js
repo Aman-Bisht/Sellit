b6a562c5ae86491e2e78e30870fc4a63b618373f
Aman <amanbisht1010@gmail.com>
2022-12-10 09:47:22 +0530

    Remove versal url

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -31,7 +31,7 @@ export const createOrder = (order) => async (dispatch) => {
         "Content-Type": "application/json",
       },
     };
-    const { data } = await axios.post("https://sellit-one.vercel.app/sellit.onrender.com/api/v1/order/new", order, config);
+    const { data } = await axios.post("https://sellit.onrender.com/api/v1/order/new", order, config);
     dispatch({ type: CREATE_ORDER_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
@@ -46,7 +46,7 @@ export const myOrders = () => async (dispatch) => {
   try {
     dispatch({ type: MY_ORDERS_REQUEST });
 
-    const { data } = await axios.get("https://sellit-one.vercel.app/sellit.onrender.com/api/v1/orders/me");
+    const { data } = await axios.get("https://sellit.onrender.com/api/v1/orders/me");
 
     dispatch({ type: MY_ORDERS_SUCCESS, payload: data.orders });
   } catch (error) {
@@ -62,7 +62,7 @@ export const getAllOrders = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_ORDERS_REQUEST });
 
-    const { data } = await axios.get("https://sellit-one.vercel.app/sellit.onrender.com/api/v1/admin/orders");
+    const { data } = await axios.get("https://sellit.onrender.com/api/v1/admin/orders");
 
     dispatch({ type: ALL_ORDERS_SUCCESS, payload: data.orders });
   } catch (error) {
@@ -83,7 +83,7 @@ export const updateOrder = (id, order) => async (dispatch) => {
       },
     };
     const { data } = await axios.put(
-      `https://sellit-one.vercel.app/sellit.onrender.com/api/v1/admin/order/${id}`,
+      `https://sellit.onrender.com/api/v1/admin/order/${id}`,
       order,
       config
     );
@@ -100,7 +100,7 @@ export const updateOrder = (id, order) => async (dispatch) => {
 export const deleteOrder = (id) => async (dispatch) => {
   try {
     dispatch({ type: DELETE_ORDER_REQUEST });
-    const { data } = await axios.delete(`https://sellit-one.vercel.app/sellit.onrender.com/api/v1/admin/order/${id}`);
+    const { data } = await axios.delete(`https://sellit.onrender.com/api/v1/admin/order/${id}`);
     dispatch({ type: DELETE_ORDER_SUCCESS, payload: data.success });
   } catch (error) {
     dispatch({
@@ -115,7 +115,7 @@ export const getOrderDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: ORDER_DETAILS_REQUEST });
 
-    const { data } = await axios.get(`https://sellit-one.vercel.app/sellit.onrender.com/api/v1/order/${id}`);
+    const { data } = await axios.get(`https://sellit.onrender.com/api/v1/order/${id}`);
 
     dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data.order });
   } catch (error) {
